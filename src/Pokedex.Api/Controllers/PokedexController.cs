bd040531d403e568e0f226af303f71958f78108f
Lucas Amaral <lucas.harmonico@acerto.com.br>
2023-01-17 11:42:13 -0300

    Adicionando Documentação Swagger

--- src/Pokedex.Api/Controllers/PokedexController.cs
+++ src/Pokedex.Api/Controllers/PokedexController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.Business.Entities;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Pokedex.Api.Controllers
 {
@@ -6,6 +8,8 @@ namespace Pokedex.Api.Controllers
     public class PokedexController : ControllerBase
     {
         [HttpPost]
+        [SwaggerOperation("Cadastrar novo pokémon")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<IActionResult> AddPokemon()
         {
 
@@ -13,6 +17,8 @@ namespace Pokedex.Api.Controllers
         }
 
         [HttpPut]
+        [SwaggerOperation("Atualizar cadastro de pokémon")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> UpdatePokemon()
         {
 
@@ -20,20 +26,26 @@ namespace Pokedex.Api.Controllers
         }
 
         [HttpDelete]
+        [SwaggerOperation("Remover pokémon")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> DeletePokemon()
         {
 
             return Ok();
         }
 
-        [HttpGet]
+        [HttpGet("{pokedexId:guid}")]
+        [SwaggerOperation("Obter pokémon por Id")]
+        [ProducesResponseType(typeof(Pokemon), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetPokemonById()
         {
 
             return Ok();
         }
 
-        [HttpGet]
+        [HttpGet("find")]
+        [SwaggerOperation("Listar pokémons")]
+        [ProducesResponseType(typeof(IEnumerable<Pokemon>), StatusCodes.Status200OK)]
         public async Task<IActionResult> FindPokemon()
         {
 
--- src/Pokedex.Api/Startup.cs
+++ src/Pokedex.Api/Startup.cs
@@ -15,7 +15,7 @@
         {
             _services.AddControllers();
             _services.AddEndpointsApiExplorer();
-            _services.AddSwaggerGen();
+            _services.AddSwaggerGen(options => options.EnableAnnotations());
         }
 
         public void ConfigureApplication(IApplicationBuilder app) //processar tudo que é app
